d22cebf51a2d5e92a7ed3fe5c3b660bcb768f5db
peter279k <peter279k@gmail.com>
2017-09-13 16:11:36 +0800

    add the runtime error code

--- main.php
+++ main.php
@@ -4,6 +4,11 @@
 * This the main PHP is to present the HTTP Request/Response example.
 */
 
+// show the runtime error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__.'/src/autoloader.php';
 
 use peter\WordPress\HttpRequest;
